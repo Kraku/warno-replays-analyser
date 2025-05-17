625d696e717a2db5973715bd50634ac786353e86
perustaja <dustincotten@gmail.com>
2025-05-16 20:50:57 -0700

    team details

---
+++ frontend/src/components/TeamDetails/TeamDetails.tsx
@@ -0,0 +1,24 @@
+import { Card, Typography, Tag } from 'antd';
+import { Team } from '../../parsers/teamsParser';
+import { TeamGamesTable } from './TeamGamesTable';
+
+export const TeamDetails = ({ team }: { team: Team }) => {
+  return (
+    <Card
+      title={
+        <div className="flex gap-2 items-center mb-2">
+          <Typography.Text>{player?.names.join(', ')}</Typography.Text>
+          <Tag bordered={false}>#{[team.player1Id, team.player2Id].join(' & ')}</Tag>
+        </div>
+      }>
+      <div>
+        <Typography.Title level={5} className="mb-2">
+          Games History<span className="text-xs text-neutral-400 ml-2">(last 10)</span>
+        </Typography.Title>
+
+        <TeamGamesTable history={team.history} />
+
+      </div>
+    </Card>
+  );
+};
